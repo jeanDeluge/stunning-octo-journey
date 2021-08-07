e68bc377a872210e20873eaf5f4acfd94531a31b
jeanDeluge <53405354+jeanDeluge@users.noreply.github.com>
2021-08-07 14:56:39 +0900

    feat : directory 정리

---
+++ src/@types/datatypes.d.ts
@@ -0,0 +1,6 @@
+export type UserId = string;
+export type Password = string;
+export type Movie = string;
+export type Theatre = string;
+export type Seats = string;
+export type TicketInfo = string;
